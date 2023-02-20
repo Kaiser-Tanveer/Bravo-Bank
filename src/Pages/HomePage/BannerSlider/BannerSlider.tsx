acb03f15d230ad9cf230b2e6be547b6dd1a823ba
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2023-02-20 10:04:59 +0600

    social icon added

--- src/Pages/HomePage/BannerSlider/BannerSlider.tsx
+++ src/Pages/HomePage/BannerSlider/BannerSlider.tsx
@@ -27,7 +27,7 @@ const BannerSlider = () => {
       <div className="container grid lg:grid-cols-2 mx-auto bg-gray-200 items-center rounded-lg mb-20">
         <img src={bannerImg} alt="main-banner" className="bg-gray-300 w-full h-full" />
         <div>
-          <p className="text-7xl text-center font-bold text-primary">
+          <p className="text-5xl md:text-7xl text-center font-bold text-primary">
             <Typewriter
               words={['Welcome to']}
               loop={5}
@@ -39,7 +39,7 @@ const BannerSlider = () => {
             />
           </p>
           <br />
-          <span className="shadow-inner hover:shadow-xl hover:animate-pulse shadow-gray-700 px-4 my-4 text-transparent bg-gradient-to-b bg-clip-text from-primary block text-center text-7xl font-bold to-gray-700 rounded-md mx-6">
+          <span className="shadow-inner hover:shadow-xl hover:animate-pulse shadow-gray-700 px-4 my-4 text-transparent bg-gradient-to-b bg-clip-text from-primary block text-center text-5xl md:text-7xl font-bold to-gray-700 rounded-md mx-6">
             Bravo Bank
           </span>
         </div>
