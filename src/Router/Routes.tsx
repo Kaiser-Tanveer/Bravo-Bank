9729ed0388342e2a18bdb2302029c20ccec49861
Md Galib Hossain <mdgalib23@gmail.com>
2023-02-23 13:09:21 +0600

    Cards added on sidebar

--- src/Router/Routes.tsx
+++ src/Router/Routes.tsx
@@ -4,19 +4,20 @@ import AccountDetails from "../Pages/AccountsDetails/AccountDetails";
 import Home from "../Pages/HomePage/Home/Home";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main />,
-        children: [
-            {
-                path: '/',
-                element: <Home />,
-                loader: () => fetch('https://bravo-bank-server.vercel.app/accountsTypes')
-            },
-            {
-                path: '/accountDetail/:accountType',
-                element: <AccountDetails />
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: () =>
+          fetch("https://bravo-bank-server.vercel.app/accountsTypes"),
+      },
+      {
+        path: "/accountDetail/:accountType",
+        element: <AccountDetails />,
+      },
+    ],
+  },
+]);
