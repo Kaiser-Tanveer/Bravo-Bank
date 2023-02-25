a10175cec99dfc98d312ec80a92f06c9e16c3dd9
Md Galib Hossain <mdgalib23@gmail.com>
2023-02-25 16:34:45 +0600

    Contact us frontend done

--- src/Router/Routes.tsx
+++ src/Router/Routes.tsx
@@ -2,36 +2,42 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import AccountDetails from "../Pages/AccountsDetails/AccountDetails";
 import CardsDetails from "../Pages/CardsDetails/CardsDetails";
+import ContactUs from "../Pages/ContactUs/ContactUs";
 import Home from "../Pages/HomePage/Home/Home";
 import LoanDetails from "../Pages/LoanDetails/LoanDetails";
 import NotFound from "../Pages/NotFound/NotFound";
 
 export const router = createBrowserRouter([
-    {
-        path: '*',
-        element: <NotFound />
-    },
-    {
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
         path: "/",
-        element: <Main />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-                loader: () => fetch("https://bravo-bank-server.vercel.app/accountsTypes"),
-            },
-            {
-                path: "/accountDetail/:accountType",
-                element: <AccountDetails />,
-            },
-            {
-                path: "/cardDetail/:cardType",
-                element: <CardsDetails />,
-            },
-            {
-                path: "/loanDetail/:loanType",
-                element: <LoanDetails />,
-            },
-        ],
-    },
+        element: <Home />,
+        loader: () =>
+          fetch("https://bravo-bank-server.vercel.app/accountsTypes"),
+      },
+      {
+        path: "/accountDetail/:accountType",
+        element: <AccountDetails />,
+      },
+      {
+        path: "/cardDetail/:cardType",
+        element: <CardsDetails />,
+      },
+      {
+        path: "/loanDetail/:loanType",
+        element: <LoanDetails />,
+      },
+      {
+        path: "/contactus",
+        element: <ContactUs />,
+      },
+    ],
+  },
 ]);
