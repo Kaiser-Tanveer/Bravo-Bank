d7f763778c1cffd35c53ae70c3a1d343fba62958
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2023-03-02 01:26:12 +0600

    User profile added

---
+++ src/Pages/UserProfile/MyLoans/MyLoans.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyLoans = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center py-10'>Your Loan goes here...</h1>
+        </div>
+    );
+};
+
+export default MyLoans;
